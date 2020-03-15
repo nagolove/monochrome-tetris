dc98bba29f5a4e07a8f80da783a291dfe17cb4ae
nagolove <dekar.own@gmail.com>
2020-03-15 21:03:45 +0300

    MinGW pkg_send.sh script adaptation.

--- pkg_send.sh
+++ pkg_send.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
-find . -name "*.lua" -or -name "*.wav" -or -name "*.png" | apack monochrome-tetris.zip
-scp monochrome-tetris.zip dekar@visualdoj.ru:/home/dekar/www/packages
-ssh dekar@visualdoj.ru /home/dekar/bin/update_index.lua
-rm monochrome-tetris.zip
+case "$(uname)" in
+Linux*)     
+    echo "Linux"
+    find . -name "*.lua" -or -name "*.wav" -or -name "*.png" ! -name "correct-filelist.lua" | apack monochrome-tetris.zip
+    scp monochrome-tetris.zip dekar@visualdoj.ru:/home/dekar/www/packages
+    ssh dekar@visualdoj.ru /home/dekar/bin/update_index.lua
+    rm monochrome-tetris.zip
+    ;;
+MINGW*)     
+    echo "MinGW"
+    find . -name "*.lua" -or -name "*.wav" -or -name "*.png" ! -name "correct-filelist.lua" > files.txt
+    ./correct-filelist.lua files.txt
+    #cat files.txt
+    /c/Program\ Files/7-Zip/7z.exe a monochrome-tetris.zip @files_.txt
+    rm files.txt
+    rm files_.txt
+    scp monochrome-tetris.zip dekar@visualdoj.ru:/home/dekar/www/packages
+    ssh dekar@visualdoj.ru /home/dekar/bin/update_index.lua
+    rm monochrome-tetris.zip
+    ;;
+*)          
+    echo "Unknown system"
+esac
